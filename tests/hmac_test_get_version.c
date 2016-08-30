68e3fd190a73c65243abf5737cf3ed160e99a58f
Joachim Metz <joachim.metz@gmail.com>
2016-08-30 07:35:24 +0200

    Worked on tests.

--- tests/hmac_test_get_version.c
+++ tests/hmac_test_get_version.c
@@ -20,7 +20,6 @@
  */
 
 #include <common.h>
-#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
@@ -28,6 +27,7 @@
 
 #include "hmac_test_libcstring.h"
 #include "hmac_test_libhmac.h"
+#include "hmac_test_macros.h"
 #include "hmac_test_unused.h"
 
 /* Tests retrieving the library version
@@ -46,11 +46,15 @@ int hmac_test_get_version(
 	          LIBHMAC_VERSION_STRING,
 	          9 );
 
-	if( result != 0 )
-	{
-		return( 0 );
-	}
+	HMAC_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
 	return( 1 );
+
+on_error:
+	return( 0 );
 }
 
 /* The main program
@@ -68,10 +72,13 @@ int main(
 	HMAC_TEST_UNREFERENCED_PARAMETER( argc )
 	HMAC_TEST_UNREFERENCED_PARAMETER( argv )
 
-	if( hmac_test_get_version() != 1 )
-	{
-		return( EXIT_FAILURE );
-	}
+	HMAC_TEST_RUN(
+	 "libhmac_get_version",
+	 hmac_test_get_version() )
+
 	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
 }
 
