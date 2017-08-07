30e3e644276b2bfcc91a8374a8af5101832fb04f
Joachim Metz <joachim.metz@gmail.com>
2017-08-07 07:43:39 +0200

    Worked on tests

--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,14 +1,10 @@
 # Tests C library functions and types.
 #
-# Version: 20170115
+# Version: 20170807
 
 $ExitSuccess = 0
 $ExitFailure = 1
 $ExitIgnore = 77
-
-$TestPrefix = Split-Path -path ${Pwd}.Path -parent
-$TestPrefix = Split-Path -path ${TestPrefix} -leaf
-$TestPrefix = ${TestPrefix}.Substring(3)
 
 $LibraryTests = "error md5_context sha1_context sha224_context sha256_context sha512_context support"
 $LibraryTestsWithInput = ""
@@ -20,7 +16,7 @@ Function RunTest
 	param( [string]$TestType )
 
 	$TestDescription = "Testing: ${TestName}"
-	$TestExecutable = "${TestToolDirectory}\${TestPrefix}_test_${TestName}.exe"
+	$TestExecutable = "${TestToolDirectory}\hmac_test_${TestName}.exe"
 
 	$Output = Invoke-Expression ${TestExecutable}
 	$Result = ${LastExitCode}
@@ -44,7 +40,15 @@ Function RunTest
 
 If (-Not (Test-Path ${TestToolDirectory}))
 {
+	$TestToolDirectory = "..\msvscpp\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
 	$TestToolDirectory = "..\vs2010\Release"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2010\VSDebug"
 }
 If (-Not (Test-Path ${TestToolDirectory}))
 {
@@ -52,7 +56,19 @@ If (-Not (Test-Path ${TestToolDirectory}))
 }
 If (-Not (Test-Path ${TestToolDirectory}))
 {
+	$TestToolDirectory = "..\vs2012\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
 	$TestToolDirectory = "..\vs2013\Release"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2013\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2015\Release"
 }
 If (-Not (Test-Path ${TestToolDirectory}))
 {
