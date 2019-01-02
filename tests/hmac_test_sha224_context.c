d2a4c00651b7c5c13e62a0053ff196822aeb44f7
Joachim Metz <joachim.metz@gmail.com>
2019-01-02 21:30:54 +0100

    Applied updates

--- tests/hmac_test_sha224_context.c
+++ tests/hmac_test_sha224_context.c
@@ -1,7 +1,7 @@
 /*
  * Library SHA-224 context type testing program
  *
- * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2019, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
