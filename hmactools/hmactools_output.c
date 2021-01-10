62e39c891c855ad9ccd225edd26730daaadef703
Joachim Metz <joachim.metz@gmail.com>
2021-01-10 13:33:33 +0100

    Applied updates

--- hmactools/hmactools_output.c
+++ hmactools/hmactools_output.c
@@ -1,7 +1,7 @@
 /*
  * Output functions
  *
- * Copyright (C) 2011-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -114,7 +114,7 @@ void hmactools_output_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (C) 2011-2020, %s.\n" ),
+	 _( "Copyright (C) 2011-2021, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
