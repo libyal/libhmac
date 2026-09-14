4708e23c442376f232dbf6b48b17356defc37262
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 16:28:52 +0200

    Worked on tests and changes for CI tests

---
+++ tests/test_tools.ps1
@@ -0,0 +1,20 @@
+# Tests tools functions and types.
+
+$ToolsTests = ""
+# TODO: read options sets from project.ini
+$OptionSets = "" -split " "
+
+. .\test_functions.ps1
+
+$TestExecutablesDirectory = GetTestExecutablesDirectory
+
+If (-Not (Test-Path ${TestExecutablesDirectory}))
+{
+	Write-Error "Missing test executables directory"
+
+	Exit ${ExitFailure}
+}
+
+$Result = ${ExitSuccess}
+
+Exit ${Result}
